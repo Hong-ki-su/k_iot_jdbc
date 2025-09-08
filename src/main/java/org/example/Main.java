3ca014cfb717e7b39b5e62a0d6a792659b936a73
Hong ki su <gisuhong258@gmail.com>
2025-09-08 09:19:53 +0900

    2025-09-08

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.example.manager.MemberManager;
+
+public class Main {
+    public static void main(String[] args) {
+        MemberManager mm = new MemberManager();
+
+        try {
+            mm.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
